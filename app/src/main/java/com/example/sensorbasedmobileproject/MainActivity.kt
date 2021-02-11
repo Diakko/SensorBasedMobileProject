67b8ade6a7b59bc39fcaa5692190661c1a80d11e
Diakko <48632853+Diakko@users.noreply.github.com>
2021-02-11 13:54:13 +0200

    Added navigation bar, and a placeholder for main fragment. Also some icons and colours

--- app/src/main/java/com/example/sensorbasedmobileproject/MainActivity.kt
+++ app/src/main/java/com/example/sensorbasedmobileproject/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.sensorbasedmobileproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainScreenFragment: Fragment
+    private lateinit var navigationFragment: Fragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        navigationFragment = NavigationFragment(this)
+        mainScreenFragment = MainFragment()
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.bottom_nav_fragment, navigationFragment)
+            replace(R.id.main_screen_fragment, mainScreenFragment)
+            commit()
+        }
     }
 }
