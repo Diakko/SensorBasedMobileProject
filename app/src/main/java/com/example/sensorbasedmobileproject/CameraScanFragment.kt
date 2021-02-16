c4968ad148160a3fdfdb0df0e4958ccdbd5d56d7
Diakko <48632853+Diakko@users.noreply.github.com>
2021-02-16 15:07:10 +0200

    Barcode scanning works and results presented as a toast on page atm.

--- app/src/main/java/com/example/sensorbasedmobileproject/CameraScanFragment.kt
+++ app/src/main/java/com/example/sensorbasedmobileproject/CameraScanFragment.kt
@@ -1,24 +1,17 @@
 package com.example.sensorbasedmobileproject
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
+import androidx.annotation.Nullable
+import androidx.fragment.app.Fragment
 import com.google.zxing.integration.android.IntentIntegrator
 
-class CameraScanFragment() : Fragment() {
 
-    lateinit var btnBarcode: Button
-    lateinit var resultTextView: TextView
-    lateinit var handler: Handler
+class CameraScanFragment() : Fragment(){
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,43 +19,28 @@ class CameraScanFragment() : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_camera_scan, container, false)
-        btnBarcode = view.findViewById(R.id.scan_barcode_button)
-        resultTextView = view.findViewById(R.id.result_textview)
+        val integrator = IntentIntegrator.forSupportFragment(this@CameraScanFragment)
 
-        btnBarcode.setOnClickListener {
-            val intentIntegrator = IntentIntegrator(activity)
-            intentIntegrator.setBeepEnabled(false)
-            intentIntegrator.setCameraId(0)
-            intentIntegrator.setPrompt("SCAN")
-            intentIntegrator.setBarcodeImageEnabled(false)
-            intentIntegrator.initiateScan()
-        }
+        integrator.setOrientationLocked(false)
+        integrator.setPrompt("Scan QR code")
+        integrator.setBeepEnabled(false)
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES)
 
+
+        integrator.initiateScan()
         return view
     }
 
-
-   override fun onActivityResult(
-        requestCode: Int,
-        resultCode: Int,
-        data: Intent?
-    ) {
+    override fun onActivityResult(requestCode: Int, resultCode: Int, @Nullable data: Intent?) {
         val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
         if (result != null) {
-            handler = Handler()
-            handler.postDelayed({
-                if (result.contents == null) {
-                    Toast.makeText(requireContext(), "cancelled", Toast.LENGTH_SHORT).show()
-                } else {
-                    Log.d("BarcodeScanner", "Scanned")
-                    Toast.makeText(requireContext(), "Scanned -> " + result.contents, Toast.LENGTH_SHORT)
-                        .show()
-                    resultTextView.text = String.format("Scanned Result: %s", result)
-                }
-            }, 10)
-        } else {
-            super.onActivityResult(requestCode, resultCode, data)
+            if (result.contents == null) {
+                Toast.makeText(context, "Cancelled", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(context, "Scanned : " + result.contents, Toast.LENGTH_LONG).show()
+            }
         }
     }
+
 
 }
