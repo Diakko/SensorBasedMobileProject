452e29b0b922bb588d0cd920659495dd549faf73
xpyx <pystynen@gmail.com>
2021-02-17 21:18:24 +0200

    copy code from roomDB bracnh

--- app/src/main/java/com/example/sensorbasedmobileproject/MainFragment.kt
+++ app/src/main/java/com/example/sensorbasedmobileproject/MainFragment.kt
@@ -1,12 +1,25 @@
 package com.example.sensorbasedmobileproject
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.sensorbasedmobileproject.repository.Repository
 
 class MainFragment : Fragment() {
+
+    private lateinit var searchResult: TextView
+    private lateinit var btnSearchFineli: Button
+    private lateinit var viewModel: MainViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -14,6 +27,56 @@ class MainFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val context = requireContext()
+
+        val repository = Repository()
+        val viewModelFactory = MainViewModelFactory(repository)
+        searchResult = view.findViewById(R.id.search_result)
+        btnSearchFineli = view.findViewById(R.id.btn_search_fineli)
+        viewModel = ViewModelProvider(this, viewModelFactory).get(MainViewModel::class.java)
+        viewModel.myResponse.observe(viewLifecycleOwner, Observer { response ->
+            if(response.isSuccessful) {
+                searchResult.text =
+
+                    "ID: " + response.body()?.get(0)?.id.toString() + "\n" +
+                            "NAME: " + response.body()?.get(0)?.name?.fi.toString() + "\n" +
+                            "DESC: " + response.body()?.get(0)?.type?.description?.fi.toString() + "\n" +
+                            "KCAL: " + response.body()?.get(0)?.energyKcal.toString()
+                Log.d("Response", response.body()?.get(0)?.id.toString())
+            } else {
+                Log.d("Response", response.errorBody().toString())
+            }
+        })
+
+        btnSearchFineli.setOnClickListener {
+            if (isNetworkAvailable(context)) {
+                viewModel.getFood("sitruuna")
+
+            }
+        }
+
+
+
+
+    }
+
+    private fun isNetworkAvailable(context: Context?): Boolean {
+        val connectivityManager =
+            context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val network = connectivityManager.activeNetwork ?: return false
+        val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
+
+        return when {
+            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+            else -> false
+        }
     }
 
 }
---
+++ app/src/main/java/com/example/sensorbasedmobileproject/model/Fineli.kt
@@ -0,0 +1,91 @@
+package com.example.sensorbasedmobileproject.model
+
+data class Fineli(
+
+    val id: Int,
+    val type: Type,
+    val name: Name,
+    val preparationMethod: PreparationMethod,
+    val ediblePortion: Int,
+    val specialDiets: List<String>,
+    val themes: List<String>,
+    val units: List<Units>,
+    val ingredientClass: IngredientClass,
+    val functionClass: FunctionClass,
+    val energy: Double,
+    val energyKcal: Double,
+    val fat: Double,
+    val protein: Double,
+    val carbohydrate: Double,
+    val alcohol: Int,
+    val organicAcids: Double,
+    val sugarAlcohol: Int,
+    val saturatedFat: Double,
+    val fiber: Double,
+    val sugar: Double,
+    val salt: Double
+)
+
+data class Abbreviation(
+
+    val fi: String,
+    val sv: String,
+    val en: String
+)
+
+
+data class Description(
+
+    val fi: String,
+    val sv: String,
+    val en: String
+)
+
+
+data class FunctionClass(
+
+    val code: String,
+    val description: Description,
+    val abbreviation: Abbreviation
+)
+
+
+data class IngredientClass(
+
+    val code: String,
+    val description: Description,
+    val abbreviation: Abbreviation
+)
+
+
+data class Name(
+
+    val fi: String,
+    val sv: String,
+    val en: String,
+    val la: String
+)
+
+
+data class PreparationMethod(
+
+    val code: String,
+    val description: Description,
+    val abbreviation: Abbreviation
+)
+
+
+data class Units(
+
+    val code: String,
+    val description: Description,
+    val abbreviation: Abbreviation,
+    val mass: Float
+)
+
+data class Type(
+
+    val code: String,
+    val description: Description,
+    val abbreviation: Abbreviation
+)
