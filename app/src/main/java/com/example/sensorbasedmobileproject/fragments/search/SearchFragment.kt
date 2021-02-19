0f3fc4048bf161219cdb66a4c294f30ef101eda4
xpyx <35662792+xpyx@users.noreply.github.com>
2021-02-19 12:50:39 +0200

    Revert "Fix api error handling"

--- app/src/main/java/com/example/sensorbasedmobileproject/fragments/search/SearchFragment.kt
+++ app/src/main/java/com/example/sensorbasedmobileproject/fragments/search/SearchFragment.kt
@@ -43,7 +43,7 @@ class SearchFragment : Fragment() {
         val adapter = ListAdapter()
         val recyclerView = view.recyclerview
         recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.VERTICAL,true)
 
         // Fineli viewmodel
         mFineliViewModel = ViewModelProvider(this).get(FineliItemViewModel::class.java)
