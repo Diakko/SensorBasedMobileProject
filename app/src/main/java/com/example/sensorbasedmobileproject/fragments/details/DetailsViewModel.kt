2eaa18e46aa2dbd53edb9456bd4b1274910c4c06
xpyx <pystynen@gmail.com>
2021-03-05 12:24:48 +0200

    start details fragment work

---
+++ app/src/main/java/com/example/sensorbasedmobileproject/fragments/details/DetailsViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.sensorbasedmobileproject.fragments.details
+
+import androidx.lifecycle.ViewModel
+
+class DetailsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
