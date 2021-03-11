fe045b04120b91dd624fc074999c307e9cbc4210
Diakko <48632853+Diakko@users.noreply.github.com>
2021-03-11 13:15:08 +0200

    moved shopping list to own fragment/menuitem and made the spinner much prettier.

--- app/src/main/java/com/example/sensorbasedmobileproject/fragments/main/OffListAdapter.kt
+++ app/src/main/java/com/example/sensorbasedmobileproject/fragments/main/OffListAdapter.kt
@@ -15,7 +15,6 @@ package com.example.sensorbasedmobileproject.fragments.main
 import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.BitmapFactory
-import android.graphics.Color
 import android.os.Handler
 import android.os.Looper
 import android.util.TypedValue
@@ -23,7 +22,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import androidx.core.content.ContextCompat
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.example.sensorbasedmobileproject.R
---
+++ app/src/main/java/com/example/sensorbasedmobileproject/fragments/shopping_list/ShoppingListFragment.kt
@@ -0,0 +1,142 @@
+package com.example.sensorbasedmobileproject.fragments.shopping_list
+
+import android.app.Activity
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.*
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.sensorbasedmobileproject.R
+import com.example.sensorbasedmobileproject.data.ShoppingListItem
+import com.example.sensorbasedmobileproject.data.ShoppingListItemDatabase
+import com.example.sensorbasedmobileproject.data.ShoppingListItemViewModel
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class ShoppingListFragment : Fragment(), AdapterView.OnItemSelectedListener {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var fragmentView: View
+    private lateinit var spinner: Spinner
+    private lateinit var shoppingListItem: String
+    private var shoppingListItemAmount: Int = 0
+    private lateinit var shoppingListItemType: String
+    private lateinit var shoppingListViewModel: ShoppingListItemViewModel
+    private val shoppingListDatabase by lazy {context?.let { ShoppingListItemDatabase.getDatabase(it)}}
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        fragmentView = inflater.inflate(R.layout.fragment_shopping_list, container, false)
+
+        // Shopping List
+        val shoppingListItemEditText = fragmentView.findViewById<EditText>(R.id.edit_text_shopping_list_item)
+        val shoppingListItemAmountEditText = fragmentView.findViewById<EditText>(R.id.edit_text_shopping_list_item_amount)
+
+        // Recyclerview
+        val recyclerAdapter =  ShoppingListAdapter(requireContext())
+        recyclerView = fragmentView.findViewById(R.id.recyclerview)
+        recyclerView.adapter = recyclerAdapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        // Add Button
+        val addButton = fragmentView.findViewById<ImageButton>(R.id.add_button)
+
+        addButton.setOnClickListener() {
+            if (shoppingListItemAmountEditText.text.isNotEmpty() && shoppingListItemEditText.text.isNotEmpty()) {
+                if (shoppingListDatabase != null) {
+                    GlobalScope.launch {
+                        shoppingListItem = shoppingListItemEditText.text.toString()
+                        shoppingListItemAmount =
+                            shoppingListItemAmountEditText.text.toString().toInt()
+                        shoppingListItemAmountEditText.text.clear()
+                        shoppingListItemEditText.text.clear()
+                        shoppingListDatabase!!.shoppingListItemDao()
+                            .insertShoppingListData(ShoppingListItem(0,
+                                shoppingListItem,
+                                shoppingListItemAmount,
+                                shoppingListItemType))
+
+                    }
+                }
+            } else {
+                Toast.makeText(requireContext(), R.string.add_item_toast, Toast.LENGTH_LONG).show()
+            }
+        }
+
+        // Keyboard hidings
+        shoppingListItemAmountEditText.setOnEditorActionListener(TextView.OnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                hideKeyboard()
+                return@OnEditorActionListener true
+            }
+            false
+        })
+        shoppingListItemEditText.setOnEditorActionListener(TextView.OnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                hideKeyboard()
+                return@OnEditorActionListener true
+            }
+            false
+        })
+
+        // Clear button
+        val clearButton = fragmentView.findViewById<Button>(R.id.clear_button)
+
+        clearButton.setOnClickListener() {
+            if (shoppingListDatabase != null) {
+                GlobalScope.launch {
+                    shoppingListDatabase!!.shoppingListItemDao().clearShoppingList()
+                }
+            }
+        }
+
+        shoppingListViewModel = ViewModelProvider(this).get(ShoppingListItemViewModel::class.java)
+        shoppingListViewModel.readAllData.observe(viewLifecycleOwner, Observer { shoppingListItem ->
+            recyclerAdapter.setData(shoppingListItem)
+        })
+
+        // Spinner item
+        spinner = fragmentView.findViewById(R.id.type_spinner)
+        spinner.onItemSelectedListener = this
+
+        ArrayAdapter.createFromResource(requireContext(), R.array.type_array, android.R.layout.simple_spinner_item).also { adapter ->
+            adapter.setDropDownViewResource(R.layout.custom_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+
+        return fragmentView
+    }
+
+
+
+    private fun Fragment.hideKeyboard() {
+        view?.let { activity?.hideKeyboard(it) }
+    }
+    private fun Context.hideKeyboard(view: View) {
+        val inputMethodManager =
+            getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    // Spinner adapter overrides
+    override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
+        shoppingListItemType = parent.getItemAtPosition(pos).toString()
+        Log.d("SHOPPING", shoppingListItemType)
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>) {
+        Log.d("SHOPPING", "nothing selected")
+    }
+}
